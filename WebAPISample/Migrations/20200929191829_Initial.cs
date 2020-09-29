2abdf656c99deda9b0472107f25e5c9ebd1d09b1
Kevin Harvey Jr <lordty@gmail.com>
2020-09-29 14:19:13 -0500

    Rebase Database

---
+++ WebAPISample/Migrations/20200929191829_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPISample.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Director = table.Column<string>(nullable: true),
+                    Genre = table.Column<string>(nullable: true),
+                    Synopsis = table.Column<string>(nullable: true),
+                    RunTime = table.Column<int>(nullable: false),
+                    ImageLocation = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
