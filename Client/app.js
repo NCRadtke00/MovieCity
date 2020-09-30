6197c50888acfdaf74265917bf30c5a6af6eab0e
NCRadtke00 <raddnc@gmail.com>
2020-09-30 11:05:08 -0500

    creating app.js functiong to updateMovie

--- Client/app.js
+++ Client/app.js
@@ -1,4 +1,34 @@
 (function ($) {
+    function UpdateMovie(e){
+      var movie = {
+      Title: this["title"].value,
+      Director: this["director"].value,
+      Genre: this["genre"].value,
+      Synopsis: this["synopsis"].value,
+      RunTime: this["run  time"].value,
+      ImageLocation: this["imagelocation"].value
+      
+    };
+    $.ajax({
+      url:"https://localhost:44325/api/movie",
+      dataType: 'json',
+      type: 'put',
+      contentType: 'application/json',
+      data: JSON.stringify(movie),
+      success: function (data, textStatus, errorThrown) {
+        $("#response pre").html(data);
+      },
+      error: function (jqXhr, textStatus, jQxhr) {
+        console.log(errorThrown);
+      }
+    }).then(function (){
+      GetMovieList();
+    })
+    e.preventDefault();
+  }
+  $("#addForm").submit(processForm);
+  
+
   function processForm(e) {
     var dict = {
       Title: this["title"].value,
@@ -6,6 +36,7 @@
       Genre: this["genre"].value,
       Synopsis: this["synopsis"].value,
       RunTime: this["run  time"].value,
+      ImageLocation: this["imagelocation"].value
     };
 
     $.ajax({
@@ -25,5 +56,45 @@
     e.preventDefault();
   }
 
-  $("#my-form").submit(processForm);
+  $("#addForm").submit(processForm);
 })(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(function(){
+  $.get("https://localhost:44325/api/movie", function(data){
+    console.log(data);
+
+    data.map(function(el){
+      $("#Movies").append('<div> Movie Title: ${el.title} </div>
+      <div> Movie Director: ${el.Director}</div> 
+      <br>');
+    })
+  })
+})
+$(function(){
+  $.get('https://localhost:44325/api/movie $(#id)', function(data){
+    console.log(data);
+
+    data.map(function(el){
+      $("#Movies").append('<div> Movie Title: ${el.title} </div>
+      <div> Movie Director: ${el.Director}</div> 
+      <br>');
+    })
+  })
+})
